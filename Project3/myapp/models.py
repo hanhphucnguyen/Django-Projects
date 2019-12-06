b404d38113eb46a24fe573fb570196566f9be163
leonguyenontario <leohpnguyen@gmail.com>
2019-12-06 13:34:44 -0500

    06122019

--- Project3/myapp/models.py
+++ Project3/myapp/models.py
@@ -5,3 +5,10 @@ class Image(models.Model):
     name = models.CharField(max_length=100)
     url = models.TextField(max_length=300)
     ima = models.ImageField(upload_to = 'hinh/',default = '')
+
+class
+
+class Projects(models.Model):
+    name = models.CharField(max_length=100,blank=True)
+    desciption = models.TextField(max_length=500,blank=True)
+    imageGalery = models.ForeignKey()
