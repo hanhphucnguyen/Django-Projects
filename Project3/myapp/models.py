ce3516f37a6ca76490acc9701738fbbb3937f440
leonguyenontario <leohpnguyen@gmail.com>
2019-12-07 14:17:36 -0500

    07122019

--- Project3/myapp/models.py
+++ Project3/myapp/models.py
@@ -8,8 +8,10 @@ class Image(models.Model):
 
 class DataForGalery(models.Model):
     img = models.ImageField(upload_to = 'hinh/',verbose_name="data image",default = '')
+    def __str__(self):
+        return self.img
   
 class Projects(models.Model):
     name = models.CharField(max_length=100,blank=True)
     desciption = models.TextField(max_length=500,blank=True)
-    imageGalery = models.ForeignKey()
+    imageGalery = models.ForeignKey(DataForGalery,on_delete=models.CASCADE)
